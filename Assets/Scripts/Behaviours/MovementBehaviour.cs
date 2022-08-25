6f08c1553ed7ef8202d007371d6c962916638f5b
Bulat Mukhamedeyev <vyvavyva@gmail.com>
2022-08-25 20:09:32 +0400

    Added PlayerInputHandler and MovementBehaviour

---
+++ Assets/Scripts/Behaviours/MovementBehaviour.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Behaviours
+{
+    [RequireComponent(typeof(CircleCollider2D))]
+    public class MovementBehaviour : MonoBehaviour
+    {
+        [SerializeField] private float _speed;
+
+        private CircleCollider2D _collider;
+
+        private void Awake()
+        {
+            _collider = GetComponent<CircleCollider2D>();
+        }
+
+        public void Move(Vector2 direction)
+        {
+            var velocity = Vector2.zero;
+            var position = (Vector2) transform.position;
+            
+            var horVelocity = direction.x * _speed * Time.deltaTime * Vector2.right;
+            var newHorPosition = position + horVelocity;
+            var verVelocity = direction.y * _speed * Time.deltaTime * Vector2.up;
+            var newVerPosition = position + verVelocity;
+            
+            if (CollisionCheck(newHorPosition)) velocity += horVelocity;
+            if (CollisionCheck(newVerPosition)) velocity += verVelocity;
+            
+            if (velocity == Vector2.zero) return;
+            transform.Translate(velocity);
+        }
+
+        private bool CollisionCheck(Vector2 newPosition)
+        {
+            Collider2D[] hits = Physics2D.OverlapCircleAll(newPosition, _collider.radius);
+
+            foreach (var hit in hits)
+            {
+                if (hit == _collider) return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Assets/Scripts/Clients/Player/PlayerInputHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Behaviours;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Clients.Player
+{
+    [RequireComponent(typeof(PlayerInput))]
+    public class PlayerInputHandler : MonoBehaviour
+    {
+        [SerializeField] private MovementBehaviour _playerCharacterMovement;
+        
+        private PlayerInput _playerInput;
+        private InputAction _movement;
+
+        private void Awake()
+        {
+            _playerInput = GetComponent<PlayerInput>();
+            _movement = _playerInput.actions["Movement"];
+        }
+
+        private void Update()
+        {
+            var movementInput = _movement.ReadValue<Vector2>();
+            if (movementInput != Vector2.zero)
+            {
+                _playerCharacterMovement.Move(movementInput);
+            }
+        }
+    }
+}
