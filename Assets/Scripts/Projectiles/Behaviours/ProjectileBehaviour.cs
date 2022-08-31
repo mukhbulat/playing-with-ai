3c854aaf4557cdddb8ded459109211c75ecaaca1
Bulat Mukhamedeyev <vyvavyva@gmail.com>
2022-08-31 20:55:52 +0400

    Added Projectiles directory with ProjectileBehaviour and ProjectileMovement. Changed enemy AI, changed attack behaviour and state

---
+++ Assets/Scripts/Projectiles/Behaviours/ProjectileBehaviour.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using Units.Controllers;
+using UnityEngine;
+
+namespace Projectiles.Behaviours
+{
+    public class ProjectileBehaviour : MonoBehaviour
+    {
+        [SerializeField] private float _lifetime = 10f;
+        private IMovable _movable;
+        private float _damage;
+        private float _colliderRadius = 0.15f;
+        private Vector3 _direction;
+        private bool _isPlayers;
+        private int _playerLayer = 1 << 7;
+        private int _enemyLayer = 1 << 8;
+        private int _objectLayer = 1 << 6;
+
+        public void Spawn(float damage, Vector3 direction, bool isPlayers = false)
+        {
+            _movable = GetComponent<IMovable>();
+            _damage = damage;
+            _direction = direction;
+            _isPlayers = isPlayers;
+            StartCoroutine(Lifetime());
+        }
+
+        private void Update()
+        {
+            _movable?.Move(_direction);
+            
+            CollisionCheck();
+        }
+
+        private void CollisionCheck()
+        {
+            var hits = Physics2D.OverlapCircleAll(transform.position, _colliderRadius);
+            
+            foreach (var hit in hits)
+            {
+                var layer = hit.gameObject.layer;
+                layer = (int) Mathf.Pow(2f, layer);
+                if (layer == _objectLayer)
+                {
+                    Destroy(gameObject);
+                }
+
+                if (_isPlayers && layer == _enemyLayer)
+                {
+                    DealDamage(hit.GetComponent<IUnit>().Damageable, _damage);
+                    Destroy(gameObject);
+                }
+
+                if (!_isPlayers && layer == _playerLayer)
+                {
+                    DealDamage(hit.GetComponent<IUnit>().Damageable, _damage);
+                    Destroy(gameObject);
+                }
+            }
+        }
+
+        private void DealDamage(IDamageable damageable, float damage)
+        {
+            damageable?.DealDamage(damage);
+        }
+        
+        private IEnumerator Lifetime()
+        {
+            yield return new WaitForSeconds(_lifetime);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Projectiles/Behaviours/ProjectileMovement.cs
@@ -0,0 +1,18 @@
+﻿using Units.Controllers;
+using UnityEngine;
+
+namespace Projectiles.Behaviours
+{
+    public class ProjectileMovement : MonoBehaviour, IMovable
+    {
+        [SerializeField] private float _speed = 15;
+        
+        public void Move(Vector2 direction)
+        {
+            direction = direction.normalized;
+
+            var velocity = direction * _speed * Time.deltaTime;
+            transform.Translate(velocity);
+        }
+    }
+}
